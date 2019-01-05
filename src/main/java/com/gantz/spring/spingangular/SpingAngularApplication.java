30678ef8262eef775d76264cf0605e46d7971401
GantZo <putrify@mail.ru>
2019-01-06 00:46:45 +0300

    Initial commit

---
+++ src/main/java/com/gantz/spring/spingangular/SpingAngularApplication.java
@@ -0,0 +1,14 @@
+package com.gantz.spring.spingangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpingAngularApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/gantz/spring/spingangular/SpingAngularApplicationTests.java
@@ -0,0 +1,17 @@
+package com.gantz.spring.spingangular;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpingAngularApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
